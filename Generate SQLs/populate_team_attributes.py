e9c42d9f4db909adbc4b2050db75c331318d23f9
Colin McDonell <colinmmcdonell@gmail.com>
2022-11-14 14:55:49 -0600

    team and team attributes sql

--- Generate SQLs/populate_team_attributes.py
+++ Generate SQLs/populate_team_attributes.py
@@ -2,7 +2,9 @@ import os
 import sqlite3
 from sqlite3 import Error
 
-create_team_attributes_table_sql = 'CREATE TABLE teams_attributes (' \
+table_name = 'team_attributes'
+
+create_team_attributes_table_sql = 'CREATE TABLE ' + table_name + '(' \
                                    'team_id INTEGER references teams(team_id),' \
                                    'date varchar(10),' \
                                    'buildUpPlaySpeed INTEGER,' \
@@ -28,6 +30,7 @@ create_team_attributes_table_sql = 'CREATE TABLE teams_attributes (' \
                                    'defenceDefenderLineClass TEXT,' \
                                    'PRIMARY KEY (team_id, date))\n\n'
 
+
 # create a database connection to a SQLite database
 conn = None
 db_file = os.getcwd()+'\\soccerData.sqlite'
@@ -35,14 +38,27 @@ db_file = os.getcwd()+'\\soccerData.sqlite'
 try:
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
-    cursor.execute('select * from Team')
+    cursor.execute('select * from Team_Attributes inner join Team on Team_Attributes.team_fifa_api_id = '
+                   'Team.team_fifa_api_id')
     rows = cursor.fetchall()
 
-    with open(os.getcwd()+'/SQL scripts/team.sql', 'w', encoding="utf-8") as file:
-        file.write(create_team_table_sql)
+    with open(os.getcwd()+'/SQL scripts/' + table_name + '.sql', 'w', encoding="utf-8") as file:
+        file.write(create_team_attributes_table_sql)
         for row in rows:
-            file.write("insert into teams (team_id, team_long_name, team_short_name) "
-                       "values (" + str(row[0]) + ",'" + row[3] + "','" + row[4] + "');\n")
+            file.write("insert into team_attributes (team_id, date, buildUpPlaySpeed,"
+                       "buildUpPlaySpeedClass, buildUpPlayDribbling, buildUpPlayDribblingClass,"
+                       "buildUpPlayPassing, buildUpPlayPassingClass, buildUpPlayPositioningClass,"
+                       "chanceCreationPassing, chanceCreationPassingClass, chanceCreationCrossing,"
+                       "chanceCreationCrossingClass, chanceCreationShooting, chanceCreationShootingClass,"
+                       "chanceCreationPositioningClass, defencePressure, defencePressureClass, defenceAggression,"
+                       "defenceAggressionClass, defenceTeamWidth, defenceTeamWidthClass, defenceDefenderLineClass) "
+                       "values (" + str(row[25]) + ",'" + row[3].split(' ')[0] + "'," + str(row[4]) + ",'"
+                       + row[5] + "'," + str(row[6]) + ",'" + row[7] + ","
+                       + str(row[8]) + ",'" + row[9] + "','" + row[10] + "',"
+                       + str(row[11]) + ",'" + row[12] + "'," + str(row[13]) + ",'"
+                       + row[14] + "'," + str(row[15]) + ",'" + row[16] + "','"
+                       + row[17] + "'," + str(row[18]) + ",'" + row[19] + "'," + row[20] + ",'"
+                       + row[21] + "'," + str(row[22]) + ",'" + row[23] + "','" + row[24] + "');\n")
 
 except Error as e:
     print(e)
